d61a839ca1a2a5895fba71d20c10cea4b98f6355
haleli amiad <haleli.am@gmail.com>
2021-05-31 13:41:14 +0200

    formatting

--- src/components/Word/Word.jsx
+++ src/components/Word/Word.jsx
@@ -1,13 +1,13 @@
 import './index.scss';
-import LetterInput from '../LetterInput/LetterInput'
-import {WordContext} from '../../context/WordContext'
-import {useContext} from 'react'
+import LetterInput from '../LetterInput/LetterInput';
+import { WordContext } from '../../context/WordContext';
+import { useContext } from 'react';
 function Word() {
-    const {word} = useContext(WordContext)
-    return (
-        <span className="word-container">
-        {word && <p>{word.split('').map((letter, index)=> <LetterInput  letterIndex={index} key={index}/>)}</p>}
-        </span>
-    )
+	const { word } = useContext(WordContext);
+	return (
+		<span className="word-container">
+			{word && <p>{word.split('').map((letter, index) => <LetterInput letterIndex={index} key={index} />)}</p>}
+		</span>
+	);
 }
-export default Word
+export default Word;
